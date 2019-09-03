2c599c793c178a9a5891cc2220ad188895fc54be
ezeVolyBlu <ezequieel.blum@gmail.com>
2019-09-02 22:34:42 -0300

    02/09

---
+++ py-web.py
@@ -0,0 +1,12 @@
+
+dia=''
+while dia=='':
+    dia = input ('ingrese dia: ')
+    if dia == 'lunes':
+        print('primer dia')
+    elif dia == 'martes':
+        print('segundo dia')
+    else: 
+        dia=''
+
+    
