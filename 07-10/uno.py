466da1b311d0bd2d9a279453df91865679847760
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-10-07 19:29:47 -0300

    Tuplas

---
+++ 07-10/uno.py
@@ -0,0 +1,10 @@
+datos = ('Ever', 'Toujours', '26')
+
+datosLista = list(datos)
+datosLista[0] = ('Valentina')
+datosTupla = tuple(datosLista)
+
+print(datos[0])
+print(datosLista[0])
+print(datos)
+print(datosTupla)
