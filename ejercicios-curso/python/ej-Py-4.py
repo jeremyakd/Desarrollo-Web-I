c49b778e06aa1bfb36cfc0385111614000bafc01
jeremyakd <jeremiasc@exo.com.ar>
2019-10-11 08:40:29 -0300

    10-10 modificado el repo

--- ejercicios-curso/python/ej-Py-4.py
+++
@@ -1,7 +0,0 @@
-#Realizar un programa que imprima 25 t�rminos de la serie 11 - 22 - 33 - 44, etc. (No se ingresan valores por teclado).
-
-aux=1
-while(aux<26):
-    print(str(11*aux))
-    print(aux)
-    aux+=1
