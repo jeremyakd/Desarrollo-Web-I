1d4549c67d8db115a309da6ca58e712a9e72bb7b
jeremyakd <jeremiasc@exo.com.ar>
2019-10-21 19:37:19 -0300

    clase 21-10

--- 10-10/webpersonal/webpersonal/urls.py
+++ 10-10/webpersonal/webpersonal/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('home', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
